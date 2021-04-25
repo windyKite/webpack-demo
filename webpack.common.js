93b727539b76f65d885b95e9a2fa7e0911d5c1a3
萧泳森 <xiaoys@ligeit.com>
2021-04-25 17:42:07 +0800

    feat: 开始持久性缓存

--- webpack.common.js
+++ webpack.common.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+  cache: true,
   entry: {
     app: "./src/index.js",
     vendor: ["react", "react-dom"],
